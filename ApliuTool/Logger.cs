3dfab5ca653b120175cd60c0f2fe037f7bb1dce9
刘安平 <apliu@foxmail.com>
2018-03-19 18:49:36 +0800

    0

---
+++ ApliuTool/Logger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace ApliuTools
+{
+    public class Logger
+    {
+        private static readonly string logPath = "~/Log/";
+        private static SemaphoreSlim sthread = new SemaphoreSlim(1);
+
+        /// <summary>
+        /// 日记记录 Web应用
+        /// </summary>
+        /// <param name="Msg"></param>
+        public static void WriteLogWeb(string Msg)
+        {
+            string filename = HttpContext.Current.Server.MapPath(logPath + DateTime.Now.ToString("yyyyMMdd") + ".txt");
+
+            try
+            {
+                sthread.Wait();
+
+                using (StreamWriter sw = new StreamWriter(filename, true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + " : " + Msg);
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            finally
+            {
+                sthread.Release();
+            }
+        }
+
+        /// <summary>
+        /// 日志记录 桌面应用
+        /// </summary>
+        /// <param name="Msg"></param>
+        public static void WriteLogDesktop(string Msg)
+        {
+            string filename = System.IO.Directory.GetCurrentDirectory() + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+
+            try
+            {
+                sthread.Wait();
+
+                using (StreamWriter sw = new StreamWriter(filename, true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + " : " + Msg);
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            finally
+            {
+                sthread.Release();
+            }
+        }
+    }
+}
---
+++ ApliuTool/SecurityHelper.cs
@@ -0,0 +1,544 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ApliuTools
+{
+    public class SecurityHelper
+    {
+        #region 通用加密算法
+        private static string key = "ApliuWeb";
+
+        /// <summary>
+        /// 公钥加密
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static string EncryptContent(Object Content)
+        {
+            string Value = Content.ToString();
+            if (string.IsNullOrEmpty(Value)) return null;
+
+            try
+            {
+                using (TripleDES provider = TripleDES.Create())
+                {
+                    key = key.Length >= 24 ? key : key.PadRight(24, '9');
+                    provider.Key = Encoding.UTF8.GetBytes(key.Substring(0, 24));
+                    provider.IV = Encoding.UTF8.GetBytes(key.Substring(8, 8));
+
+                    byte[] encryptedBinary = EncryptTextToMemory(Value, provider.Key, provider.IV);
+                    return Convert.ToBase64String(encryptedBinary);
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 公钥解密
+        /// </summary>
+        /// <param name="Content"></param>
+        /// <returns></returns>
+        public static string DecryptConten(Object Content)
+        {
+            string Value = Content.ToString();
+            if (string.IsNullOrEmpty(Value)) return Value;
+
+            try
+            {
+                using (TripleDES provider = TripleDES.Create())
+                {
+                    key = key.Length >= 24 ? key : key.PadRight(24, '9');
+                    provider.Key = Encoding.UTF8.GetBytes(key.Substring(0, 24));
+                    provider.IV = Encoding.UTF8.GetBytes(key.Substring(8, 8));
+
+                    byte[] buffer = Convert.FromBase64String(Value);
+                    return DecryptTextFromMemory(buffer, provider.Key, provider.IV);
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private static byte[] EncryptTextToMemory(string data, byte[] key, byte[] iv)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, TripleDES.Create().CreateEncryptor(key, iv), CryptoStreamMode.Write))
+                {
+                    byte[] toEncrypt = Encoding.Unicode.GetBytes(data);
+                    cs.Write(toEncrypt, 0, toEncrypt.Length);
+                    cs.FlushFinalBlock();
+                }
+
+                return ms.ToArray();
+            }
+        }
+
+        private static string DecryptTextFromMemory(byte[] data, byte[] key, byte[] iv)
+        {
+            using (var ms = new MemoryStream(data))
+            {
+                using (var cs = new CryptoStream(ms, TripleDES.Create().CreateDecryptor(key, iv), CryptoStreamMode.Read))
+                {
+                    using (var sr = new StreamReader(cs, Encoding.Unicode))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private static readonly byte[] IvBytes = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
+
+        /// <summary>
+        /// 哈希加密算法
+        /// </summary>
+        /// <param name="hashAlgorithm"> 所有加密哈希算法实现均必须从中派生的基类 </param>
+        /// <param name="input"> 待加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        private static string HashEncrypt(HashAlgorithm hashAlgorithm, string input, Encoding encoding)
+        {
+            var data = hashAlgorithm.ComputeHash(encoding.GetBytes(input));
+
+            return BitConverter.ToString(data).Replace("-", "");
+        }
+
+        /// <summary>
+        /// 验证哈希值
+        /// </summary>
+        /// <param name="hashAlgorithm"> 所有加密哈希算法实现均必须从中派生的基类 </param>
+        /// <param name="unhashedText"> 未加密的字符串 </param>
+        /// <param name="hashedText"> 经过加密的哈希值 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        private static bool VerifyHashValue(HashAlgorithm hashAlgorithm, string unhashedText, string hashedText,
+            Encoding encoding)
+        {
+            return string.Equals(HashEncrypt(hashAlgorithm, unhashedText, encoding), hashedText,
+                StringComparison.OrdinalIgnoreCase);
+        }
+
+        #endregion 通用加密算法
+
+        #region 哈希加密算法
+
+        #region MD5 算法
+
+        /// <summary>
+        /// MD5 加密
+        /// </summary>
+        /// <param name="input"> 待加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string MD5Encrypt(string input, Encoding encoding)
+        {
+            return HashEncrypt(MD5.Create(), input, encoding);
+        }
+
+        /// <summary>
+        /// 验证 MD5 值
+        /// </summary>
+        /// <param name="input"> 未加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static bool VerifyMD5Value(string input, Encoding encoding)
+        {
+            return VerifyHashValue(MD5.Create(), input, MD5Encrypt(input, encoding), encoding);
+        }
+
+        #endregion MD5 算法
+
+        #region SHA1 算法
+
+        /// <summary>
+        /// SHA1 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string SHA1Encrypt(string input, Encoding encoding)
+        {
+            return HashEncrypt(SHA1.Create(), input, encoding);
+        }
+
+        /// <summary>
+        /// 验证 SHA1 值
+        /// </summary>
+        /// <param name="input"> 未加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static bool VerifySHA1Value(string input, Encoding encoding)
+        {
+            return VerifyHashValue(SHA1.Create(), input, SHA1Encrypt(input, encoding), encoding);
+        }
+
+        #endregion SHA1 算法
+
+        #region SHA256 算法
+
+        /// <summary>
+        /// SHA256 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string SHA256Encrypt(string input, Encoding encoding)
+        {
+            return HashEncrypt(SHA256.Create(), input, encoding);
+        }
+
+        /// <summary>
+        /// 验证 SHA256 值
+        /// </summary>
+        /// <param name="input"> 未加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static bool VerifySHA256Value(string input, Encoding encoding)
+        {
+            return VerifyHashValue(SHA256.Create(), input, SHA256Encrypt(input, encoding), encoding);
+        }
+
+        #endregion SHA256 算法
+
+        #region SHA384 算法
+
+        /// <summary>
+        /// SHA384 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string SHA384Encrypt(string input, Encoding encoding)
+        {
+            return HashEncrypt(SHA384.Create(), input, encoding);
+        }
+
+        /// <summary>
+        /// 验证 SHA384 值
+        /// </summary>
+        /// <param name="input"> 未加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static bool VerifySHA384Value(string input, Encoding encoding)
+        {
+            return VerifyHashValue(SHA256.Create(), input, SHA384Encrypt(input, encoding), encoding);
+        }
+
+        #endregion SHA384 算法
+
+        #region SHA512 算法
+
+        /// <summary>
+        /// SHA512 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string SHA512Encrypt(string input, Encoding encoding)
+        {
+            return HashEncrypt(SHA512.Create(), input, encoding);
+        }
+
+        /// <summary>
+        /// 验证 SHA512 值
+        /// </summary>
+        /// <param name="input"> 未加密的字符串 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static bool VerifySHA512Value(string input, Encoding encoding)
+        {
+            return VerifyHashValue(SHA512.Create(), input, SHA512Encrypt(input, encoding), encoding);
+        }
+
+        #endregion SHA512 算法
+
+        #region HMAC-MD5 加密
+
+        /// <summary>
+        /// HMAC-MD5 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="key"> 密钥 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string HMACSMD5Encrypt(string input, string key, Encoding encoding)
+        {
+            return HashEncrypt(new HMACMD5(encoding.GetBytes(key)), input, encoding);
+        }
+
+        #endregion HMAC-MD5 加密
+
+        #region HMAC-SHA1 加密
+
+        /// <summary>
+        /// HMAC-SHA1 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="key"> 密钥 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string HMACSHA1Encrypt(string input, string key, Encoding encoding)
+        {
+            return HashEncrypt(new HMACSHA1(encoding.GetBytes(key)), input, encoding);
+        }
+
+        #endregion HMAC-SHA1 加密
+
+        #region HMAC-SHA256 加密
+
+        /// <summary>
+        /// HMAC-SHA256 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="key"> 密钥 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string HMACSHA256Encrypt(string input, string key, Encoding encoding)
+        {
+            return HashEncrypt(new HMACSHA256(encoding.GetBytes(key)), input, encoding);
+        }
+
+        #endregion HMAC-SHA256 加密
+
+        #region HMAC-SHA384 加密
+
+        /// <summary>
+        /// HMAC-SHA384 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="key"> 密钥 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string HMACSHA384Encrypt(string input, string key, Encoding encoding)
+        {
+            return HashEncrypt(new HMACSHA384(encoding.GetBytes(key)), input, encoding);
+        }
+
+        #endregion HMAC-SHA384 加密
+
+        #region HMAC-SHA512 加密
+
+        /// <summary>
+        /// HMAC-SHA512 加密
+        /// </summary>
+        /// <param name="input"> 要加密的字符串 </param>
+        /// <param name="key"> 密钥 </param>
+        /// <param name="encoding"> 字符编码 </param>
+        /// <returns></returns>
+        public static string HMACSHA512Encrypt(string input, string key, Encoding encoding)
+        {
+            return HashEncrypt(new HMACSHA512(encoding.GetBytes(key)), input, encoding);
+        }
+
+        #endregion HMAC-SHA512 加密
+
+        #endregion 哈希加密算法
+
+        #region 对称加密算法
+
+        #region Des 加解密
+
+        /// <summary>
+        /// DES 加密
+        /// </summary>
+        /// <param name="input"> 待加密的字符串 </param>
+        /// <param name="key"> 密钥（8位） </param>
+        /// <returns></returns>
+        public static string DESEncrypt(string input, string key)
+        {
+            try
+            {
+                var keyBytes = Encoding.UTF8.GetBytes(key);
+                //var ivBytes = Encoding.UTF8.GetBytes(iv);
+
+                var des = DES.Create();
+                des.Mode = CipherMode.ECB; //兼容其他语言的 Des 加密算法
+                des.Padding = PaddingMode.Zeros; //自动补 0
+
+                using (var ms = new MemoryStream())
+                {
+                    var data = Encoding.UTF8.GetBytes(input);
+
+                    using (var cs = new CryptoStream(ms, des.CreateEncryptor(keyBytes, IvBytes), CryptoStreamMode.Write)
+                        )
+                    {
+                        cs.Write(data, 0, data.Length);
+                        cs.FlushFinalBlock();
+                    }
+
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            catch
+            {
+                return input;
+            }
+        }
+
+        /// <summary>
+        /// DES 解密
+        /// </summary>
+        /// <param name="input"> 待解密的字符串 </param>
+        /// <param name="key"> 密钥（8位） </param>
+        /// <returns></returns>
+        public static string DESDecrypt(string input, string key)
+        {
+            try
+            {
+                var keyBytes = Encoding.UTF8.GetBytes(key);
+                //var ivBytes = Encoding.UTF8.GetBytes(iv);
+
+                var des = DES.Create();
+                des.Mode = CipherMode.ECB; //兼容其他语言的Des加密算法
+                des.Padding = PaddingMode.Zeros; //自动补0
+
+                using (var ms = new MemoryStream())
+                {
+                    var data = Convert.FromBase64String(input);
+
+                    using (var cs = new CryptoStream(ms, des.CreateDecryptor(keyBytes, IvBytes), CryptoStreamMode.Write)
+                        )
+                    {
+                        cs.Write(data, 0, data.Length);
+
+                        cs.FlushFinalBlock();
+                    }
+
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+            catch
+            {
+                return input;
+            }
+        }
+
+        #endregion Des 加解密
+
+        #endregion 对称加密算法
+
+        #region 非对称加解密算法
+
+        /// <summary>
+        /// 生成 RSA 公钥和私钥
+        /// </summary>
+        /// <param name="publicKey"> 公钥 </param>
+        /// <param name="privateKey"> 私钥 </param>
+        public static void GenerateRSAKeys(out string publicKey, out string privateKey)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                publicKey = rsa.ToXmlString(false);
+                privateKey = rsa.ToXmlString(true);
+            }
+        }
+
+        /// <summary>
+        /// RSA 加密
+        /// </summary>
+        /// <param name="publickey"> 公钥 </param>
+        /// <param name="content"> 待加密的内容 </param>
+        /// <returns> 经过加密的字符串 </returns>
+        public static string RSAEncrypt(string publickey, string content)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publickey);
+            var cipherbytes = rsa.Encrypt(Encoding.UTF8.GetBytes(content), false);
+
+            return Convert.ToBase64String(cipherbytes);
+        }
+
+        /// <summary>
+        /// RSA 解密
+        /// </summary>
+        /// <param name="privatekey"> 私钥 </param>
+        /// <param name="content"> 待解密的内容 </param>
+        /// <returns> 解密后的字符串 </returns>
+        public static string RSADecrypt(string privatekey, string content)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(privatekey);
+            var cipherbytes = rsa.Decrypt(Convert.FromBase64String(content), false);
+
+            return Encoding.UTF8.GetString(cipherbytes);
+        }
+
+        #endregion 非对称加密算法
+
+        #region Base64加解密算法
+        /// <summary>
+        /// Base64加密，采用utf8编码方式加密
+        /// </summary>
+        /// <param name="source">待加密的明文</param>
+        /// <returns>加密后的字符串</returns>
+        public static string Base64Encode(string source)
+        {
+            return Base64Encode(Encoding.UTF8, source);
+        }
+
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="encodeType">加密采用的编码方式</param>
+        /// <param name="source">待加密的明文</param>
+        /// <returns></returns>
+        public static string Base64Encode(Encoding encodeType, string source)
+        {
+            string encode = string.Empty;
+            byte[] bytes = encodeType.GetBytes(source);
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = source;
+            }
+            return encode;
+        }
+
+        /// <summary>
+        /// Base64解密，采用utf8编码方式解密
+        /// </summary>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string Base64Decode(string result)
+        {
+            return Base64Decode(Encoding.UTF8, result);
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="encodeType">解密采用的编码方式，注意和加密时采用的方式一致</param>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string Base64Decode(Encoding encodeType, string result)
+        {
+            string decode = string.Empty;
+            byte[] bytes = Convert.FromBase64String(result);
+            try
+            {
+                decode = encodeType.GetString(bytes);
+            }
+            catch
+            {
+                decode = result;
+            }
+            return decode;
+        }
+        #endregion
+
+    }
+}
