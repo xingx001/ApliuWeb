e304f118895748a2692f4f5cf7b1b85a33f8f833
18779182730 <apliu@foxmail.com>
2018-04-01 00:12:47 +0800

    游戏优化关卡

--- ApliuWeb/obj/Release/Package/PackageTmp/OldWeb/pins/js/game.js
+++ ApliuWeb/obj/Release/Package/PackageTmp/OldWeb/pins/js/game.js
@@ -1,12 +1,12 @@
 ﻿var gamestart;
 var time = -1;
-var guan=1;
+var guan = 1;
 var t;
 // 计时
 function showtime() {
-	time++;
-	t = setTimeout("showtime()", 1000);
-}showtime();
+    time++;
+    t = setTimeout("showtime()", 1000);
+} showtime();
 function MC(t) {
     if (!(this instanceof MC)) return new MC(t);
     if (this.ctx = t.fill ? t : t = (t.getContext ? t : document.querySelector(t)).getContext("2d"), !this.fill) {
@@ -22,26 +22,26 @@ function MC(t) {
             }, i = [],
             o = [];
         e.M = e.L = e.Z = e.B = e.A = e.C = e.Q = {};
-        for (var a in t)("function" == typeof t[a] ? o : i).push(a);
-        i.forEach(function(t) {
-            e[t] = function(e) {
+        for (var a in t) ("function" == typeof t[a] ? o : i).push(a);
+        i.forEach(function (t) {
+            e[t] = function (e) {
                 return arguments.length ? (this.ctx[t] = e, this) : this.ctx[t]
             }
-        }), o.forEach(function(t) {
-            e[t] = function() {
+        }), o.forEach(function (t) {
+            e[t] = function () {
                 return this.ctx[t].apply(this.ctx, arguments) || this
             }
-        }), Object.keys(n).forEach(function(t) {
+        }), Object.keys(n).forEach(function (t) {
             e[n[t]] = e[t]
         })
     }
 }
 
 function gohw() {
-    
+
 }
- document.getElementById("gohw"),
-function(t, e, n) {
+document.getElementById("gohw"),
+function (t, e, n) {
     function i(t) {
         return t %= M, 0 > t ? t + M : t
     }
@@ -82,13 +82,13 @@ function(t, e, n) {
                         }
                     T = !1
                 }
-                T ? (e = w * D(T.a), e = w * A(T.a) + n.sqrt(4 * p * p - e * e), a(t, e), y = "over", c.save().font("bold 18px sans-serif").textAlign("right").fillStyle("#f00").fillText("失败了！", h - 10, g - 60).restore(), stop(),gamestart=l, gameover(F-1)) : 1 > e ? a(t, u) : (q.push(i(t - z)), o(t), y = "idle", j || (y = "pass", ("jfcz-data", '{"aa":' + (F + 1) + "}")
-				
+                T ? (e = w * D(T.a), e = w * A(T.a) + n.sqrt(4 * p * p - e * e), a(t, e), y = "over", c.save().font("bold 18px sans-serif").textAlign("right").fillStyle("#f00").fillText("失败了！", h - 10, g - 60).restore(), stop(), gamestart = l, gameover(F - 1)) : 1 > e ? a(t, u) : (q.push(i(t - z)), o(t), y = "idle", j || (y = "pass", ("jfcz-data", '{"aa":' + (F + 1) + "}")
+
 , c.save().font("bold 18px sans-serif").textAlign("right").fillStyle("#060").fillText("恭喜过关！", h - 10, g - 60).fillText("稍后一秒。", h - 10, g - 30).restore(), setTimeout(l, 1e3)))
-				guan=F;
+                guan = F;
             }
-			
-            c.save().font("bold 5px sans-serif").textAlign("left").fillStyle("#000").fillText(" 时间:"+(time)+" 分数:"+F*F*F, 10, g - 60).font("bold 48px sans-serif").textAlign("center").fillStyle("#fff").fillText(F, m, d).restore(), N(r)
+
+            c.save().font("bold 5px sans-serif").textAlign("left").fillStyle("#000").fillText(" 时间:" + (time) + " 分数:" + F * F * F, 10, g - 60).font("bold 48px sans-serif").textAlign("center").fillStyle("#fff").fillText(F, m, d).restore(), N(r)
         }
     }
 
@@ -98,8 +98,8 @@ function(t, e, n) {
 
     function f(t) {
         if ("idle" == y) {
-            if ("img" === t.target.tagName.toLowerCase() && "gohwimg" === t.target.id) return void("touchstart" === t.type || t.preventDefault());
-            switch (T || ["mousedown", "touchstart", "MSPointerDown", "pointerdown"].forEach(function(n) {
+            if ("img" === t.target.tagName.toLowerCase() && "gohwimg" === t.target.id) return void ("touchstart" === t.type || t.preventDefault());
+            switch (T || ["mousedown", "touchstart", "MSPointerDown", "pointerdown"].forEach(function (n) {
                 n != t.type && e.removeEventListener(n, f, !1)
             }), t.type) {
                 case "mousedown":
@@ -120,7 +120,7 @@ function(t, e, n) {
 
     function l() {
         if (!y || "over" == y || "pass" == y) {
-             "pass" == y && F++, y = "idle";
+            "pass" == y && F++, y = "idle";
             var t;
             t = R.length, t = t >= F ? R[F - 1] : [R[t - 1][0], R[t - 1][1] + F - t], P = {
                 b: t[0],
@@ -146,118 +146,136 @@ function(t, e, n) {
             [8, 13]
         ],
         I = {
-            normal: function(t) {
+            normal: function (t) {
                 return M * (t % b) / b
             },
-            anti: function(t) {
+            anti: function (t) {
                 return M * (1 - t % b / b)
             }
         };
-    v = 50, p = 10, w = 140, b = 5e3, x = 200, S = 100, y = "", M = 2 * n.PI, C = n.PI / 2, A = n.cos, D = n.sin, c = MC("#stage"), u = c.ctx, h = u.canvas.width, g = u.canvas.height, m = h / 2, d = .4 * g, c.fillStyle("#000").strokeStyle("#000"), c.font("12px sans-serif").textAlign("center").textBaseline("middle"), L = [0, C], q = L.concat(), E = 4 * n.asin(p / 2 / w), ["mousedown", "touchstart", "MSPointerDown", "pointerdown"].forEach(function(t) {
+    v = 50, p = 10, w = 140, b = 5e3, x = 200, S = 100, y = "", M = 2 * n.PI, C = n.PI / 2, A = n.cos, D = n.sin, c = MC("#stage"), u = c.ctx, h = u.canvas.width, g = u.canvas.height, m = h / 2, d = .4 * g, c.fillStyle("#000").strokeStyle("#000"), c.font("12px sans-serif").textAlign("center").textBaseline("middle"), L = [0, C], q = L.concat(), E = 4 * n.asin(p / 2 / w), ["mousedown", "touchstart", "MSPointerDown", "pointerdown"].forEach(function (t) {
         e.addEventListener(t, f, !1)
     });
     var N = t.requestAnimationFrame || t.webkitRequestAnimationFrame || t.mozRequestAnimationFrame || t.msRequestAnimationFrame;
-    ! function() {
+    ! function () {
         var t = ("jfcz-data");
         if (t) {
             try {
                 t = JSON.parse(t)
-            } catch (e) {}
+            } catch (e) { }
             t && t.aa && (F = 0 | t.aa)
         }
     }(), l()
 }(window, document, Math);
-function gameover(level){
-	clearTimeout(t);
-	//$(".gameoverscore").text("你一共闯关了"+level+"关!");
-	var inputgamephp = "php/inputgame.php?game=pins"+"&score="+(guan*guan*guan-1)+"&time="+time+"&max="+guan;
-	$.ajax({
-			url:inputgamephp,
-			type: "GET",
-			dataType: "text",
-			success: function(savegame){
-								//返回值包括html以及body标签
-			var isOK=savegame.substring(17,savegame.length-16);
-								//不完整
-				
-				if(isOK=="0") $("#results").text("战绩已保存!");
-				if(isOK=="1")
-				{
-					$("#results").text("未登录无法保存战绩！");
-					$('#islogin').show();
-				}
-				if(isOK=="2") $("#results").text("连接服务器失败！");
-				if(isOK=="3") $("#results").text("相同战绩已保存！");
-         }
+function gameover(level) {
+    clearTimeout(t);
+    var params = {
+        "gamename": "Pins",
+        "score": (guan * guan * guan - 1),
+        "usetime": time,
+        "stage": guan
+    };
+    var data = ApliuCommon.getoptions("Post", params, "");
+    $.when(ApliuCommon.HttpSend("/api/common/setgamedata", data)).then(function (rst) {
+        if (rst.code == "1") {
+            $.confirm("登录后可以保存成绩", "是否登录", function () {
+                window.location.href = "/User/Login";
+            }, function () {
+                //取消操作
+            });
+        }
+    }, function (rst) {
+
     });
-	
-	$("#display2").text("分数: " + (guan*guan*guan-1));
-	$("#display3").text("用时: " + time +"s");
-	$("#display4").text("关数: " + guan);
-	
-	setTimeout("$('#gameover').show()",1000);
-	
+
+    //$(".gameoverscore").text("你一共闯关了"+level+"关!");
+    //var inputgamephp = "php/inputgame.php?game=pins"+"&score="+(guan*guan*guan-1)+"&time="+time+"&max="+guan;
+    //$.ajax({
+    //		url:inputgamephp,
+    //		type: "GET",
+    //		dataType: "text",
+    //		success: function(savegame){
+    //							//返回值包括html以及body标签
+    //		var isOK=savegame.substring(17,savegame.length-16);
+    //							//不完整
+
+    //			if(isOK=="0") $("#results").text("战绩已保存!");
+    //			if(isOK=="1")
+    //			{
+    //				$("#results").text("未登录无法保存战绩！");
+    //				$('#islogin').show();
+    //			}
+    //			if(isOK=="2") $("#results").text("连接服务器失败！");
+    //			if(isOK=="3") $("#results").text("相同战绩已保存！");
+    //     }
+    //});
+
+    $("#display2").text("分数: " + (guan * guan * guan - 1));
+    $("#display3").text("用时: " + time + "s");
+    $("#display4").text("关数: " + guan);
+
+    setTimeout("$('#gameover').show()", 1000);
+
 }
-function startGame(){
-	$("#gameover").hide();
+function startGame() {
+    $("#gameover").hide();
     showtime();
-	gamestart();
+    gamestart();
 }
-function share(){
-	window.location.href="../login/login.html?id=pins";
+function share() {
+    window.location.href = "../login/login.html?id=pins";
 }
 
-function clickMore(){
-	window.location.href="../cloudmain/game.html"; 
+function clickMore() {
+    window.location.href = "../cloudmain/game.html";
 }
 
 
 function getCookie(name)//取cookies函数       
 {
-    var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
-     if(arr != null) return (arr[2]); return null;
+    var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr != null) return (arr[2]); return null;
 }
 function delCookie(name)//删除cookie
 {
     var exp = new Date();
     exp.setTime(exp.getTime() - 1);
-    var cval=getCookie(name);
-    if(cval!=null) document.cookie= name + "="+cval+";expires="+exp.toGMTString()+"; path=/";
+    var cval = getCookie(name);
+    if (cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString() + "; path=/";
 }
 function logout() {
-	delCookie("cur_username");//删除cookie
-	$("#logout").hide();
-	$("#username").hide();
-	$("#register").show();
-	$("#login").show();
-	
+    delCookie("cur_username");//删除cookie
+    $("#logout").hide();
+    $("#username").hide();
+    $("#register").show();
+    $("#login").show();
+
 }
-$(document).ready(function(){
-	$('#islogin').hide();
-	var cur_username = getCookie("cur_username");
-	if(cur_username==null){
-		$("#logout").hide();
-		$("#username").hide();
-	}
-	else{
-		$("#register").hide();
-		$("#login").hide();
-		$("#username").html("欢迎 "+cur_username+" 到来！");
-	}
+$(document).ready(function () {
+    $('#islogin').hide();
+    var cur_username = getCookie("cur_username");
+    if (cur_username == null) {
+        $("#logout").hide();
+        $("#username").hide();
+    }
+    else {
+        $("#register").hide();
+        $("#login").hide();
+        $("#username").html("欢迎 " + cur_username + " 到来！");
+    }
 });
 
-function GetRequest() { 
-	var url = location.search; //获取url中"?"符后的字串 
-	var theRequest = new Object(); 
-	if (url.indexOf("?") != -1) { 
-		var str = url.substr(1); 
-		strs = str.split("&"); 
-		for(var i = 0; i < strs.length; i ++) { 
-		theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]); 
-		} 
-	} 
-	return theRequest; 
-}var Request = new Object(); 
-Request = GetRequest(); 
+function GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串 
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
+} var Request = new Object();
+Request = GetRequest();
 
-	
