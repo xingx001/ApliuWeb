65be6858c33b1958407fb92bbc09e16f9199d967
Apliu <apliu@foxmail.com>
2018-08-02 11:28:06 +0800

    增加SignalR实时通信框架的支持

---
+++ ApliuWeb/Models/ServerHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ApliuWeb
+{
+    public class ServerHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void SendMsg(string message)
+        {
+            //调用所有客户端的sendMessage方法
+            Clients.All.sendMessage(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), System.Threading.Thread.CurrentThread.ManagedThreadId,
+                ApliuTools.Web.HYRequest.GetIP(), message);
+        }
+    }
+}
